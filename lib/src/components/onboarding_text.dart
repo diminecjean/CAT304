dea4026fb1da6b7deafefce46f027656e60352dd
Looi Wei En <looi.weien02@student.usm.my>
2023-12-26 00:03:30 +0800

    add ui components

---
+++ lib/src/components/onboarding_text.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class OnboardingText extends StatelessWidget {
+  OnboardingText({
+    super.key,
+    required this.inputTextTitle,
+    required this.inputTextContent,
+  });
+  String inputTextTitle;
+  String inputTextContent;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: Text(
+              inputTextTitle,
+              textAlign: TextAlign.center,
+              style: GoogleFonts.roboto(
+                textStyle: TextStyle(
+                  color: Color.fromARGB(255, 143, 49, 44),
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: Text(
+              inputTextContent,
+              textAlign: TextAlign.center,
+              style: GoogleFonts.roboto(
+                textStyle: TextStyle(
+                  color: Color.fromARGB(255, 143, 49, 44),
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.w300,
+                  height: 1.5,
+                ),
+              ),
+            ),
+          ),
+        ]));
+  }
+}
