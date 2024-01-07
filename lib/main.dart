0f94116827df0284e1b287b3222ea9a4ac0595c6
Looi Wei En <looi.weien02@student.usm.my>
2024-01-07 23:03:10 +0800

    modify appbar theme and routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:express_all/src/config/config_color.dart';
-import 'package:express_all/src/pages/addChild_page.dart';
+import 'package:express_all/src/pages/account/addChild_page.dart';
 import 'package:express_all/src/pages/home_page.dart';
-import 'package:express_all/src/pages/login_page.dart';
+import 'package:express_all/src/pages/account/login_page.dart';
 import 'package:express_all/src/pages/onboarding_page.dart';
-import 'package:express_all/src/pages/signup_page.dart';
-import 'package:express_all/src/pages/chooseAccountType_page.dart';
-import 'package:express_all/src/pages/signup_children_page.dart';
-import 'package:express_all/src/pages/addChild_page.dart';
+import 'package:express_all/src/pages/account/signup_page.dart';
+import 'package:express_all/src/pages/account/chooseAccountType_page.dart';
+import 'package:express_all/src/pages/account/signup_children_page.dart';
+import 'package:express_all/src/pages/facial_expression_recognition/facialExpression_page.dart';
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -24,37 +24,46 @@ class MyApp extends StatelessWidget {
       title: 'Express All',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primaryColor: ConfigColor.getMaterialColor(Color(0xFF8F312C)),
-        // textTheme: GoogleFonts.robotoTextTheme(
-        //   Theme.of(context).textTheme,
-        // ),
-        // primarySwatch: ConfigColor.getMaterialColor(Color(0xFF8F312C)),
-        // TODO
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            fontSize: 32.0,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'YourHeadingFont', // Replace with your heading font
-            color: Colors.black, // Change to your preferred color
-          ),
-          displayMedium: TextStyle(
-            fontSize: 24.0,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'YourTitleFont', // Replace with your title font
-            color: Colors.black87, // Change to your preferred color
-          ),
-          // Add more text styles for different types of text here
+        scaffoldBackgroundColor: const Color.fromARGB(255, 245, 241, 233),
+        primaryColor: ConfigColor.getMaterialColor(const Color(0xFF8F312C)),
+        textTheme: GoogleFonts.robotoTextTheme(
+          Theme.of(context).textTheme,
         ),
+        // primarySwatch: ConfigColor.getMaterialColor(Color(0xFF8F312C)),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color.fromARGB(255, 245, 241, 233),
+          elevation: 0.0,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+        ),
+        // TODO
+        // textTheme: const TextTheme(
+        //   displayLarge: TextStyle(
+        //     fontSize: 32.0,
+        //     fontWeight: FontWeight.bold,
+        //     fontFamily: 'YourHeadingFont', // Replace with your heading font
+        //     color: Colors.black, // Change to your preferred color
+        //   ),
+        //   displayMedium: TextStyle(
+        //     fontSize: 24.0,
+        //     fontWeight: FontWeight.bold,
+        //     fontFamily: 'YourTitleFont', // Replace with your title font
+        //     color: Colors.black87, // Change to your preferred color
+        //   ),
+        //   // Add more text styles for different types of text here
+        // ),
       ),
       initialRoute: '/Onboarding',
       routes: <String, WidgetBuilder>{
         '/Onboarding': (BuildContext context) => const OnBoardingPage(),
         '/Login': (BuildContext context) => const LoginPage(),
-        '/ParentSignUp': (BuildContext context) => ParentSignUpPage(),
-        '/ChildSignUp': (BuildContext context) => ChildSignUpPage(),
-        '/Home': (BuildContext context) => HomePage(),
+        '/ParentSignUp': (BuildContext context) => const ParentSignUpPage(),
+        '/ChildSignUp': (BuildContext context) => const ChildSignUpPage(),
+        '/Home': (BuildContext context) => const HomePage(),
         '/ChooseAccountType': (BuildContext context) => const ChooseAccountTypePage(),
-        '/AddChild': (BuildContext context) => AddChildPage()
+        '/AddChild': (BuildContext context) => const AddChildPage(),
+        '/FacialExpression': (BuildContext context) => const FacialExpressionPage(),
       },
     );
   }
