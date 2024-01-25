a14f126cb4b65aeac73242028bb7f31a01901674
Looi Wei En <looi.weien02@student.usm.my>
2024-01-25 13:07:08 +0800

    update colors

--- lib/src/config/style/constants.dart
+++ lib/src/config/style/constants.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 const primaryColor = Color(0xFF8F312C);
 const secondaryColor = Color(0xFFFDEBA8);
 const backgroundColor = Color(0xFFF5F1E9);
-const greenColor = Color(0xFF159D09);
-const redColor = Color(0xFFE92E30);
+const greenColor = Color(0xFF0A7D00);
+const redColor = Color(0xFFAB0205);
 const grayColor = Color(0xFFC1C1C1);
 const blackColor = Color(0xFF101010);
-const orangeColor = Color(0xFFF09A59);
+const orangeColor = Color(0xFFCA5700);
 // const kPrimaryGradient = LinearGradient(
 //   colors: [Color(0xFF46A0AE), Color(0xFF00FFCB)],
 //   begin: Alignment.centerLeft,
