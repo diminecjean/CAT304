12137dfc89605aeac97c1c289febc99f50fe8fd7
Looi Wei En <looi.weien02@student.usm.my>
2024-01-29 00:18:11 +0800

    update code logic for data handling

--- lib/src/pages/account/addChild_page.dart
+++ lib/src/pages/account/addChild_page.dart
@@ -1,16 +1,52 @@
+import 'package:express_all/src/components/toast.dart';
 import 'package:express_all/src/config/style/constants.dart';
+import 'package:express_all/src/pages/account/signup_children_page.dart';
+import 'package:express_all/src/services/auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 
 // TODO: should have a skip button
 // TODO: should have a backward button
-class AddChildPage extends StatelessWidget {
-  const AddChildPage({super.key});
+class AddChildPage extends StatefulWidget {
+  AddChildPage({Key? key}) : super(key: key);
+
+  @override
+  _AddChildPageState createState() => _AddChildPageState();
+}
+
+class _AddChildPageState extends State<AddChildPage> {
+  final FirebaseAuthService _auth = FirebaseAuthService();
+  final TextEditingController _emailController = TextEditingController();
+
+  bool isAddingChild = false;
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Add your child'),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text("Add Your Child"),
+            TextButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith<Color?>(
+                  (Set<MaterialState> states) {
+                    return backgroundColor;
+                  },
+                ),
+              ),
+              onPressed: () => {Navigator.pushNamed(context, "/ParentMenu")},
+              child: const Text('Skip', style: TextStyle(color: primaryColor)),
+            ),
+          ],
+        ),
         backgroundColor: Colors.white,
         foregroundColor: Colors.black, // Text color
         elevation: 0, // Removes the shadow
@@ -21,10 +57,13 @@ class AddChildPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             const SizedBox(height: 20), // Spacing at the top
-            Image.asset('assets/images/children.png',
-                height: 100), // Replace with your asset image for child icon
+            Image.asset(
+              'assets/images/children.png',
+              height: 100,
+            ), // Replace with your asset image for child icon
             const SizedBox(height: 20),
             TextField(
+              controller: _emailController,
               decoration: InputDecoration(
                 filled: true,
                 fillColor:
@@ -49,18 +88,26 @@ class AddChildPage extends StatelessWidget {
                     const Size(double.infinity, 50), // Button width and height
               ),
               onPressed: () {
-                // Action to add the child
+                _addChild();
               },
               child: const Text('Add'),
             ),
             const Spacer(), // Pushes the below content to the bottom of the screen
             TextButton(
               onPressed: () {
-                Navigator.pushNamed(context, '/ChildSignUp');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ChildSignUpPage(
+                      fromPage: "parent",
+                    ),
+                  ),
+                );
               },
               child: const Text(
-                  "Your child don't have an account yet? Create one now!",
-                  style: TextStyle(color: primaryColor)),
+                "Your child don't have an account yet? Create one now!",
+                style: TextStyle(color: primaryColor),
+              ),
             ),
             ElevatedButton(
               style: ElevatedButton.styleFrom(
@@ -74,7 +121,14 @@ class AddChildPage extends StatelessWidget {
                     const Size(double.infinity, 50), // Button width and height
               ),
               onPressed: () {
-                Navigator.pushNamed(context, '/ChildSignUp');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ChildSignUpPage(
+                      fromPage: "parent",
+                    ),
+                  ),
+                );
               },
               child: const Text('Create Child Account'),
             ),
@@ -82,5 +136,29 @@ class AddChildPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void _addChild() async {
+    setState(() {
+      isAddingChild = true;
+    });
+
+    String childEmail = _emailController.text;
+    User? user = await _auth.getCurrentUser();
+    String? parentEmail = user?.email;
+    bool success = await _auth.addChildtoParent(childEmail, parentEmail!);
+    Logger().i(success);
+    if (success) {
+      Logger().i("Child is successfully added");
+      showToast(message: "Child is successfully added");
+      Navigator.pushNamed(context, "/ParentMenu");
+    } else {
+      Logger().e("Fail to add child");
+      showToast(message: "Fail to add child");
+    }
+
+    setState(() {
+      isAddingChild = false;
+    });
   }
 }
