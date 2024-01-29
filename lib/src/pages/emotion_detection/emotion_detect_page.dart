8343432d471e0f34e18db2e22e5f76ad60570d73
ahdeanlau <ahdeanlau@gmail.com>
2024-01-30 00:45:51 +0800

    Able to successfully call GCP Vision API

--- lib/src/pages/emotion_detection/emotion_detect_page.dart
+++ lib/src/pages/emotion_detection/emotion_detect_page.dart
@@ -1,9 +1,10 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:express_all/src/services/gcp_vision_api.dart';
 
 class EmotionDetectionPage extends StatefulWidget {
-  const EmotionDetectionPage({super.key});
+  const EmotionDetectionPage({Key? key}) : super(key: key);
 
   @override
   _EmotionDetectionPageState createState() => _EmotionDetectionPageState();
@@ -11,14 +12,26 @@ class EmotionDetectionPage extends StatefulWidget {
 
 class _EmotionDetectionPageState extends State<EmotionDetectionPage> {
   XFile? _image;
+  final api = GcpVisionApi("assets/credentials/charged-kiln-411801-f5e5cdcaee01.json"); // replace with your actual path
+  String? _emotionResult;
+
+  @override
+  void initState() {
+    super.initState();
+    api.initialize();
+  }
 
   Future pickImage() async {
     final ImagePicker picker = ImagePicker();
     final XFile? image = await picker.pickImage(source: ImageSource.gallery);
 
-    setState(() {
-      _image = image;
-    });
+    if (image != null) {
+      var imageFile = File(image.path);
+      _emotionResult = await api.detectFaces(imageFile);
+      setState(() {
+        _image = image;
+      });
+    }
   }
 
   @override
@@ -28,22 +41,23 @@ class _EmotionDetectionPageState extends State<EmotionDetectionPage> {
         title: const Text('Emotion Detection'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            _image == null
-                ? const Text('No image selected.')
-                : Image.file(File(_image!.path)),
-            const Text(''),
-            const Text('Emotion: Happy'),
-            const Text(''),
-            ElevatedButton(
-              onPressed: pickImage,
-              child: const Text('Upload Photo'),
-            ),
-
-            // Add more widgets here for further processing or displaying results
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _image == null
+                  ? const Text('No image selected.')
+                  : Image.file(File(_image!.path)),
+              const Text(''),
+              Text('Emotion: $_emotionResult'),
+              const Text(''),
+              ElevatedButton(
+                onPressed: pickImage,
+                child: const Text('Upload Photo'),
+              ),
+              // Add more widgets here for further processing or displaying results
+            ],
+          ),
         ),
       ),
     );
