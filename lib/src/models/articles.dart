546658855d2c20b16729c1120304c18a17b1eb31
Looi Wei En <looi.weien02@student.usm.my>
2024-02-03 12:13:58 +0800

    add url to article

--- lib/src/models/articles.dart
+++ lib/src/models/articles.dart
@@ -1,12 +1,13 @@
 class ArticlesModel {
   final int id;
-  final String title, caption, image;
+  final String title, caption, image, url;
 
   ArticlesModel({
     required this.id,
     required this.title,
     required this.caption,
     required this.image,
+    required this.url,
   });
 }
 
@@ -16,13 +17,16 @@ const List articles = [
     "title": "The Art of Autism Exhibition",
     "caption":
         "Experience creativity and talent of individuals on autism spectrum.",
-    "image": "assets/images/autisticEvent1.jpeg"
+    "image": "assets/images/autisticEvent1.jpeg",
+    "url": "https://the-art-of-autism.com/",
   },
   {
     "id": 1,
     "title": "Parenting a Child on the Autism Spectrum",
     "caption": "Tips for parenting a child on the autism spectrum challenges.",
     "image": "assets/images/autisticEvent2.png",
+    "url":
+        "https://www.helpguide.org/articles/autism-learning-disabilities/parenting-a-child-with-autism.htm",
   },
   {
     "id": 2,
@@ -30,5 +34,7 @@ const List articles = [
     "caption":
         "Exploring challenges and strategies for individuals with sensory processing disorder.",
     "image": "assets/images/autisticEvent3.jpg",
+    "url":
+        "https://www.understood.org/en/learning-thinking-differences/child-learning-disabilities/sensory-processing-issues/understanding-sensory-processing-issues",
   },
 ];
