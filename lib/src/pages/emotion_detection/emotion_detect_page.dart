ab239f209be6b953d78c694734b33346c53fe938
Looi Wei En <looi.weien02@student.usm.my>
2024-02-21 12:31:03 +0800

    update UI

--- lib/src/pages/emotion_detection/emotion_detect_page.dart
+++ lib/src/pages/emotion_detection/emotion_detect_page.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:express_all/src/config/style/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:express_all/src/services/gcp_vision_api.dart';
@@ -13,8 +14,10 @@ class EmotionDetectionPage extends StatefulWidget {
 
 class _EmotionDetectionPageState extends State<EmotionDetectionPage> {
   XFile? _image;
-  final api = GcpVisionApi("assets/credentials/charged-kiln-411801-f5e5cdcaee01.json"); // replace with your actual path
+  final api = GcpVisionApi(
+      "assets/credentials/charged-kiln-411801-f5e5cdcaee01.json"); // replace with your actual path
   String? _emotionResult;
+  bool _isLoading = false;
 
   @override
   void initState() {
@@ -28,12 +31,16 @@ class _EmotionDetectionPageState extends State<EmotionDetectionPage> {
       final XFile? image = await picker.pickImage(source: ImageSource.gallery);
 
       if (image != null) {
+        setState(() {
+          _isLoading = true;
+        });
         var imageFile = File(image.path);
         var emotionResult = await api.detectFaces(imageFile);
         if (mounted) {
           setState(() {
             _image = image;
             _emotionResult = emotionResult;
+            _isLoading = false;
           });
         }
       }
@@ -52,25 +59,115 @@ class _EmotionDetectionPageState extends State<EmotionDetectionPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Emotion Detection'),
+        title: const Text(
+          "Emotion Detection",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 26,
+            fontWeight: FontWeight.w900,
+            color: primaryColor,
+          ),
+        ),
       ),
       body: Center(
         child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              _image == null
-                  ? const Text('No image selected.')
-                  : Image.file(File(_image!.path)),
-              const Text(''),
-              Text('Dominant Emotion: $_emotionResult'),
-              const Text(''),
-              ElevatedButton(
-                onPressed: pickImage,
-                child: const Text('Upload Photo'),
-              ),
-              // Add more widgets here for further processing or displaying results
-            ],
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _image == null
+                    ? Column(
+                        // mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                            const Text(
+                              "Upload an image to detect emotions",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600,
+                                color: primaryColor,
+                              ),
+                            ),
+                            const SizedBox(height: 40),
+                            Image.asset('assets/images/upload_picture.png',
+                                height: 320),
+                            const SizedBox(height: 30),
+                            const Text('No image selected.'),
+                          ])
+                    : _isLoading
+                        ? const CircularProgressIndicator(
+                            color: primaryColor,
+                          )
+                        : Column(
+                            children: [
+                              const Text(
+                                "Your image has been uploaded successfully!",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600,
+                                  color: primaryColor,
+                                ),
+                              ),
+                              Image.file(File(_image!.path)),
+                              const Text(''),
+                              Text(
+                                'Dominant Emotion: $_emotionResult',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600,
+                                  color: primaryColor,
+                                ),
+                              ),
+                              const Text(''),
+                            ],
+                          ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(primaryColor),
+                    elevation: MaterialStateProperty.all<double>(2),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                  onPressed: pickImage,
+                  child: Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Wrap(
+                      alignment: WrapAlignment.center,
+                      runAlignment: WrapAlignment.center,
+                      crossAxisAlignment: WrapCrossAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.upload_rounded,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                        SizedBox(
+                            width:
+                                10), // Add some spacing between the logo and the text
+                        _image == null
+                            ? Text(
+                                'Upload A Photo',
+                                style: TextStyle(color: Colors.white),
+                              )
+                            : Text(
+                                'Upload Another Photo',
+                                style: TextStyle(color: Colors.white),
+                              )
+                      ],
+                    ),
+                  ),
+                ),
+
+                // Add more widgets here for further processing or displaying results
+              ],
+            ),
           ),
         ),
       ),
